df97fd875a3e739fa5106231980457c2eb3a30e7
adelnizamutdinov <stiggpwnz@gmail.com>
2017-08-24 11:28:04 +0300

    initial

---
+++ app/src/main/java/adeln/countdown/MainActivity.kt
@@ -0,0 +1,11 @@
+package adeln.countdown
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
