0c676bf0c11bb02cf0a50f78fe27f0fc0e19c77b
adelnizamutdinov <stiggpwnz@gmail.com>
2017-09-06 19:00:28 +0300

    date picker stuff

--- app/src/main/java/adeln/countdown/MainActivity.kt
+++ app/src/main/java/adeln/countdown/MainActivity.kt
@@ -2,6 +2,11 @@ package adeln.countdown
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.app.FragmentManager
+import android.app.FragmentTransaction
 import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Context
@@ -10,6 +15,7 @@ import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceActivity
 import org.jetbrains.anko.ctx
+import org.joda.time.LocalDate
 
 inline fun <reified T> Context.componentName(): ComponentName =
     ComponentName(ctx, T::class.java)
@@ -26,13 +32,47 @@ class MainActivity : Activity() {
     }
 }
 
+class DatePickerFragment : DialogFragment() {
+
+    companion object {
+        fun new(ld: LocalDate): DatePickerFragment =
+            DatePickerFragment().also {
+                it.arguments = Bundle().also {
+                    it.putSerializable("date", ld)
+                }
+            }
+    }
+
+    fun dateArg(): LocalDate =
+        arguments.getSerializable("date") as LocalDate
+
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        val listener = DatePickerDialog.OnDateSetListener { _, y, m, d ->
+            LocalDate(y, m, d)
+        }
+
+        val dt = dateArg()
+        return DatePickerDialog(ctx, listener, dt.year, dt.monthOfYear, dt.dayOfMonth)
+    }
+}
+
+fun FragmentTransaction.commitNow(mgr: FragmentManager) {
+    commit()
+    mgr.executePendingTransactions()
+}
+
 @SuppressLint("ExportedPreferenceActivity")
 class SettingsActivity : PreferenceActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         preferenceScreen = preferenceManager.createPreferenceScreen(ctx)
         val p = Preference(ctx).apply {
-
+            setOnPreferenceClickListener {
+                fragmentManager.beginTransaction()
+                    .add(DatePickerFragment.new(LocalDate.now()), "fuck")
+                    .commitNow(fragmentManager)
+                true
+            }
         }
         preferenceScreen.addPreference(p)
     }
